3c57ec474f56713c484aa13fc2e01ac0a3c04a40
Xavier Francisco <xavier.n.francisco@gmail.com>
2015-05-25 17:29:11 +0100

    Parte C acabada

--- parteC.hs
+++ parteC.hs
@@ -1,5 +1,8 @@
 import Cp
+import BTree
 import Probability
+
+import Control.Parallel.Strategies
 
 pcataList :: (Either () (a, b) -> Dist b) -> [a] -> Dist b
 pcataList g = mfoldr (curry (g.i2)) ((g.i1) ()) where
@@ -22,4 +25,16 @@ test07 = gene (i2("a",["b"])) == D [(["a","b"],0.95),(["b"],0.05)]
 
 ataque = transmitir (words "Vamos atacar hoje")
 
+parmap :: (a -> b) -> [a] -> Eval [b]
+parmap f [] = return []
+parmap f (a:lt) = do
+    a' <- rpar (f a)
+    lt' <- parmap f lt
+    return (a':lt')
 
+parBTreeMap :: (a -> b) -> (BTree a) -> Eval (BTree b)
+parBTreeMap f Empty                  = return Empty
+parBTreeMap f (Node (a, (bt1, bt2))) = do a'   <- rpar (f a)
+                                          bt1' <- parBTreeMap f bt1
+                                          bt2' <- parBTreeMap f bt2
+                                          return (Node (a', (bt1', bt2')))
